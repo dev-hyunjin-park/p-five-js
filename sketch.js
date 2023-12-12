b747cf0c1a9c118a88fa3155b1938d5b72e973ec
박현진 <hyunjin@MacBook-Air.local>
2023-12-12 23:29:45 +0900

    웹캠 이미지를 ml5로 분류하기

--- sketch.js
+++ sketch.js
@@ -1,35 +1,42 @@
 let mobilenet;
 
-let puffin;
+let video;
+let label = "";
 
 function gotResults(error, results) {
   if (error) {
     console.error(error);
   } else {
-    console.log(results);
-    let label = results[0].label;
-    let confidence = results[0].confidence;
-    fill(0);
-    textSize(64);
-    text(label, 10, height - 50);
-    createP(label);
-    createP(confidence);
+    label = results[0].label;
+
+    mobilenet.predict(gotResults);
   }
 }
 
 function modelReady() {
-  mobilenet.predict(puffin, gotResults);
+  console.log("Model is ready!");
+  mobilenet.predict(gotResults); // loop
 }
 
-function imageReady() {
-  image(puffin, 0, 0, width, height);
-}
+// function imageReady() {
+//   image(puffin, 0, 0, width, height);
+// }
 
 function setup() {
-  createCanvas(640, 480);
-  puffin = createImg("images/puffin.jpeg", imageReady);
-  puffin.hide();
+  createCanvas(640, 550);
+  video = createCapture(VIDEO);
+  video.hide();
   background(0);
 
-  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+  mobilenet = ml5.imageClassifier("MobileNet", video, modelReady);
+  // 이미지 분류 모델로 MobileNet을 사용한다, 분류 대상, 콜백 함수
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0);
+  // 캡쳐된 비디오 장면을 실제 캔버스 위에 그린다
+  fill(255);
+  textSize(32);
+  text(label, 10, height - 20);
 }
