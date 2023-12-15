93c13fa7621209c8cc12c1c12dcc8b7ef02b347e
박현진 <dev.hyunjin.park@gmail.com>
2023-12-15 11:48:32 +0900

    Saving and Loading

--- mobilenet.js
+++ mobilenet.js
@@ -1,14 +1,23 @@
 let mobilenet;
-let predictor;
+let classifier;
 
 let video;
-let value = 0;
-let slider;
-let addBtn;
+let label = "loading model";
+let HappyButton;
+let SadButton;
 let trainButton;
+let saveButton;
 
 function modelReady() {
   console.log("Model is ready!");
+  // 로드 파일 - 하나의 파일을 로드하면 자동적으로 같은 위치에 있는 나머지 파일을 찾도록 설정되어있다
+  classifier.load("model.json", customModelReady);
+}
+
+function customModelReady() {
+  console.log("Custom Model is ready!");
+  label = "model ready";
+  classifier.classify(gotResults);
 }
 
 function videoReady() {
@@ -16,22 +25,22 @@ function videoReady() {
 }
 
 // training process에 대해 report back to me
-function whileTraining(loss) {
-  if (loss == null) {
-    console.log("Training Complete");
-    predictor.predict(gotResults);
-  } else {
-    console.log(loss);
-  }
-}
+// function whileTraining(loss) {
+//   if (loss == null) {
+//     console.log("Training Complete");
+//     classifier.classify(gotResults);
+//   } else {
+//     console.log(loss);
+//   }
+// }
 
 function gotResults(error, result) {
   if (error) {
     console.error(error);
   } else {
     console.log(result);
-    value = result.value;
-    predictor.predict(gotResults);
+    label = result[0].label;
+    classifier.classify(gotResults);
   }
 }
 
@@ -41,29 +50,31 @@ function setup() {
   video.hide();
   background(0);
   mobilenet = ml5.featureExtractor("MobileNet", modelReady);
-  predictor = mobilenet.regression(video, videoReady);
+  classifier = mobilenet.classification(video, videoReady);
 
-  slider = createSlider(0, 1, 0.5, 0.01);
-
-  addBtn = createButton("add example image");
-  addBtn.mousePressed(function () {
-    predictor.addImage(slider.value());
-  });
-
-  trainButton = createButton("train");
-  trainButton.mousePressed(function () {
-    predictor.train(whileTraining);
+  // HappyButton = createButton("happy");
+  // HappyButton.mousePressed(function () {
+  //   classifier.addImage("happy");
+  // });
+  // SadButton = createButton("sad");
+  // SadButton.mousePressed(function () {
+  //   classifier.addImage("sad");
+  // });
+  // trainButton = createButton("train");
+  // trainButton.mousePressed(function () {
+  //   classifier.train(whileTraining);
+  // });
+  saveButton = createButton("save");
+  saveButton.mousePressed(function () {
+    classifier.save();
   });
 }
 
 function draw() {
   background(0);
-  image(video, 0, 0, 640, 550);
-  rectMode(CENTER);
-  fill(255, 0, 255);
-  rect(value * width, height / 2, 50, 50);
-
+  image(video, 0, 0);
+  // 캡쳐된 비디오 장면을 실제 캔버스 위에 그린다
   fill(255);
   textSize(32);
-  text(value, 10, height - 20);
+  text(label, 10, height - 20);
 }
